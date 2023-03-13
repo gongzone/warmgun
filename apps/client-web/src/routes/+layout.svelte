02e8734290ccc76f285feacac07cf104b4997bf7
dnjsqhwo <dnjsqhwo@gmail.com>
2023-03-14 07:42:09 +0900

    feat: tailwind css + skeleton setting

---
+++ apps/client-web/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '@fontsource/noto-sans-kr/400.css';
+	import '@fontsource/noto-sans-kr/100.css';
+	import '@fontsource/noto-serif-kr/400.css';
+	import '@fontsource/concert-one/400.css';
+
+	import '../styles/themes/theme-default.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../styles/global.postcss';
+</script>
