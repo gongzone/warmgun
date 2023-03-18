75293da89e6f4875693c103eb7a3e7363e762ea8
dnjsqhwo <dnjsqhwo@gmail.com>
2023-03-18 11:02:07 +0900

    feat: editor ui 구현

--- apps/client-web/src/components/Editor/Editor.svelte
+++ apps/client-web/src/components/Editor/Editor.svelte
@@ -3,6 +3,14 @@
 
 	import { onMount, onDestroy } from 'svelte';
 	import EditorJS from '@editorjs/editorjs';
+	import Header from '@editorjs/header';
+	import Quote from '@editorjs/quote';
+	import Alert from 'editorjs-alert';
+	import NestedList from '@editorjs/nested-list';
+	import ImageTool from '@editorjs/image';
+	import Marker from '@editorjs/marker';
+	import Underline from '@editorjs/underline';
+	import InlineCode from '@editorjs/inline-code';
 
 	import AutosizedTextarea from '$components/@base/Input/AutoSizedTextarea.svelte';
 
@@ -16,8 +24,57 @@
 
 	onMount(() => {
 		editor = new EditorJS({
-			holder: 'editorjs',
-			placeholder: '당신의 이야기를 들려주세요.'
+			holder: 'editor',
+			placeholder: '당신의 이야기를 들려주세요.',
+			tools: {
+				header: {
+					class: Header,
+					inlineToolbar: true,
+					config: {
+						placeholder: 'Enter a header',
+						levels: [2, 3, 4],
+						defaultLevel: 2
+					}
+				},
+				quote: {
+					class: Quote,
+					inlineToolbar: true
+				},
+				alert: {
+					class: Alert,
+					inlineToolbar: true,
+					shortcut: 'CMD+SHIFT+A',
+					config: {
+						defaultType: 'primary',
+						messagePlaceholder: 'Enter something'
+					}
+				},
+				list: {
+					class: NestedList,
+					inlineToolbar: true,
+					config: {
+						defaultStyle: 'unordered'
+					}
+				},
+				image: {
+					class: ImageTool,
+					config: {
+						endpoints: {
+							byFile: 'http://localhost:8008/uploadFile', // Your backend file uploader endpoint
+							byUrl: 'http://localhost:8008/fetchUrl' // Your endpoint that provides uploading by Url
+						}
+					}
+				},
+				Marker: {
+					class: Marker
+				},
+				inlineCode: {
+					class: InlineCode
+				},
+				underline: {
+					class: Underline
+				}
+			}
 		});
 	});
 
@@ -44,5 +101,5 @@
 		<hr />
 	</div>
 
-	<div id="editorjs" class="px-[0.75rem]" />
+	<div id="editor" class="px-[0.75rem]" />
 </div>
