dfa47b5d6b750c6f1b326065ca8a6a2e98cb2de7
dnjsqhwo <dnjsqhwo@gmail.com>
2023-05-01 02:51:29 +0900

    feat: publish 기능 구현

--- warmgun/src/routes/write/draft/[draftId]/+page.server.ts
+++ warmgun/src/routes/write/draft/[draftId]/+page.server.ts
@@ -1,10 +1,13 @@
 import type { PageServerLoad, Actions } from './$types';
 import { error, fail, redirect } from '@sveltejs/kit';
+import { nanoid } from 'nanoid';
 
 import { db } from '$lib/server/db';
 import { validateFormData } from '$lib/server/validation';
 import { deleteDraftDto } from '$lib/server/schemas/delete-draft.dto';
 import { saveDraftDto } from '$lib/server/schemas/save-draft.dto';
+import { publishSchema } from '$lib/server/schemas/publish.dto';
+import { meilisearchClient } from '$lib/server/meilisearch';
 
 export const ssr = false;
 
@@ -93,5 +96,59 @@ export const actions: Actions = {
 		});
 
 		return { isSuccess: true, message: '초고를 성공적으로 저장했습니다. 🎉' };
+	},
+	publish: async ({ request, locals }) => {
+		const formData = await request.formData();
+
+		const validated = validateFormData(formData, publishSchema);
+		if (!validated.success) {
+			return fail(400, { isSuccess: false, message: validated.errorMessage });
+		}
+
+		const { title, subTitle, body, coverImage, tags } = {
+			...validated.data,
+			coverImage: validated.data.coverImage ?? null,
+			body: JSON.parse(validated.data.body),
+			tags: validated.data.tags.split(',')
+		};
+
+		const slug = `@${locals.user?.username}/${title
+			.trim()
+			.toLowerCase()
+			.replace(' ', '-')}-${nanoid()}`;
+
+		const article = await db.article.create({
+			data: {
+				title,
+				subTitle,
+				body,
+				coverImage,
+				tags: {
+					connectOrCreate: tags.map((tag: string) => ({
+						where: { name: tag },
+						create: { name: tag }
+					}))
+				},
+				slug,
+				author: { connect: { id: locals.user?.id } }
+			},
+			include: { tags: true }
+		});
+
+		await meilisearchClient.index('articles').addDocuments([
+			{
+				id: article.id,
+				title: article.title,
+				subTitle: article.subTitle,
+				tags: article.tags.map((tag) => tag.name)
+			}
+		]);
+
+		await meilisearchClient.index('tags').addDocuments(
+			article.tags.map((tag) => ({
+				id: tag.id,
+				name: tag.name
+			}))
+		);
 	}
 };
