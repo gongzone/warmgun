bba482760f4078228d6dc735eb81122dc3fb0eef
dnjsqhwo <dnjsqhwo@gmail.com>
2023-05-17 04:44:24 +0900

    feat: auth refactoring

---
+++ real/src/routes/(base)/auth/login/+page.server.ts
@@ -0,0 +1,69 @@
+import type { Actions, PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import { z } from 'zod';
+import argon2 from 'argon2';
+
+import { prisma } from '$lib/server/db';
+import { validate } from '$lib/server/validation';
+import { generateTokens } from '$lib/server/jwt';
+import { setAuthCookies } from '$lib/server/cookie';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	if (locals.user) {
+		throw redirect(302, '/');
+	}
+};
+
+export const actions: Actions = {
+	default: async ({ cookies, request }) => {
+		const formData = await request.formData();
+
+		const validated = validate(formData, loginSchema());
+		if (!validated.success) {
+			return fail(400, { message: validated.errorMessage });
+		}
+
+		const { username, password } = validated.data;
+
+		const foundUser = await prisma.user.findUnique({ where: { username } });
+
+		if (!foundUser) {
+			return fail(401, { message: '잘못된 아이디입니다.' });
+		}
+
+		const matched = await argon2.verify(foundUser.password, password);
+
+		if (!matched) {
+			return fail(401, { message: '잘못된 비밀번호입니다.' });
+		}
+
+		const { accessToken, refreshToken } = await generateTokens({
+			userId: foundUser.id,
+			username: foundUser.username
+		});
+
+		const hashedRefreshToken = await argon2.hash(refreshToken);
+
+		const token = await prisma.token.create({
+			data: {
+				refreshToken: hashedRefreshToken,
+				user: { connect: { id: foundUser.id } }
+			}
+		});
+
+		setAuthCookies(cookies, {
+			tokenId: token.id,
+			accessToken,
+			refreshToken
+		});
+
+		throw redirect(303, '/');
+	}
+};
+
+function loginSchema() {
+	return z.object({
+		username: z.string().min(1, '아이디를 입력해주세요.'),
+		password: z.string().min(1, '비밀번호를 입력해주세요.')
+	});
+}
---
+++ real/src/routes/(base)/auth/signup/+page.server.ts
@@ -0,0 +1,99 @@
+import type { Actions, PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import { z } from 'zod';
+import argon2 from 'argon2';
+
+import { validate } from '$lib/server/validation';
+import { prisma } from '$lib/server/db';
+import { generateTokens } from '$lib/server/jwt';
+import { setAuthCookies } from '$lib/server/cookie';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	if (locals.user) {
+		throw redirect(302, '/');
+	}
+};
+
+export const actions: Actions = {
+	default: async ({ cookies, request }) => {
+		const formData = await request.formData();
+		const validated = validate(formData, signupSchema());
+
+		if (!validated.success) {
+			return fail(400, { message: validated.errorMessage });
+		}
+
+		const { username, password, email } = validated.data;
+
+		const foundUser = await prisma.user.findFirst({
+			where: {
+				OR: [{ username }, { email }]
+			}
+		});
+
+		if (foundUser) {
+			return fail(409, { message: '해당 아이디 혹은 이메일을 가진 사용자가 이미 존재합니다.' });
+		}
+
+		const hashedPassword = await argon2.hash(password);
+
+		const user = await prisma.user.create({
+			data: {
+				username,
+				password: hashedPassword,
+				email,
+				profile: {
+					create: { nickname: username, field: '블로거', bio: `안녕하세요. ${username}입니다.` }
+				},
+				drafts: { create: {} }
+			}
+		});
+
+		/* Todo: meilisearch Add Index */
+
+		const { accessToken, refreshToken } = await generateTokens({
+			userId: user.id,
+			username: user.username
+		});
+
+		const hashedRefreshToken = await argon2.hash(refreshToken);
+
+		const token = await prisma.token.create({
+			data: {
+				refreshToken: hashedRefreshToken,
+				user: { connect: { id: user.id } }
+			}
+		});
+
+		setAuthCookies(cookies, {
+			tokenId: token.id,
+			accessToken,
+			refreshToken
+		});
+
+		throw redirect(303, '/');
+	}
+};
+
+function signupSchema() {
+	return z
+		.object({
+			username: z.string().regex(/^[A-Za-z]{1}[A-Za-z0-9]{4,19}$/, {
+				message: '아이디 생성 규칙을 확인하세요.'
+			}),
+			password: z
+				.string()
+				.regex(
+					/^(?!((?:[A-Za-z]+)|(?:[~!@#$%^&*()_+=]+)|(?:[0-9]+))$)[A-Za-z\d~!@#$%^&*()_+=]{8,20}$/,
+					{
+						message: '비밀번호 생성 규칙을 확인하세요.'
+					}
+				),
+			confirm: z.string(),
+			email: z.string().email({ message: '이메일 규칙에 따라 작성하여 주십시오.' })
+		})
+		.refine((data) => data.password === data.confirm, {
+			message: '비밀번호가 일치하지 않습니다.',
+			path: ['confirm']
+		});
+}
