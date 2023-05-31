9181fc5e28f3574d906d79d5236333c7a7059f04
dnjsqhwo <dnjsqhwo@gmail.com>
2023-05-31 17:25:14 +0900

    fix: 아티클 커버이미지 부재시 fallback 이미지 추가

--- real/src/components/Article/ArticleItem/ArticleItemImage/ArticleItemImage.svelte
+++ real/src/components/Article/ArticleItem/ArticleItemImage/ArticleItemImage.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+	import ImageIcon from '$components/@icons/ImageIcon.svelte';
+
 	export let slug: string;
 	export let coverImage: string | null | undefined;
 </script>
 
 <a href={slug}>
-	<img
-		src={coverImage ? `${coverImage}?w=560&h=420&q=80&f=webp` : ''}
-		alt="cover"
-		class="aspect-[560/420] rounded-2xl shadow-xl"
-	/>
+	{#if coverImage}
+		<img
+			src={`${coverImage}?w=560&h=420&q=80&f=webp`}
+			alt="cover"
+			class="aspect-[560/420] rounded-2xl shadow-xl"
+		/>
+	{:else}
+		<div
+			class="flex items-center justify-center aspect-[560/420] rounded-2xl shadow-xl bg-surface-700"
+		>
+			<ImageIcon class="w-20 h-20" />
+		</div>
+	{/if}
 </a>
