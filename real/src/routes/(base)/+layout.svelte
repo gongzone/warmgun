627ed48d2d1d4b7077f295d2c94df7fa681279a2
dnjsqhwo <dnjsqhwo@gmail.com>
2023-06-07 03:28:26 +0900

    chore: refactoring

--- real/src/routes/(base)/+layout.svelte
+++ real/src/routes/(base)/+layout.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-	import { AppShell } from '@skeletonlabs/skeleton';
-
-	/* App Frames */
 	import AppHeader from './_AppHeader/AppHeader.svelte';
 	import AppFooter from './_AppFooter/AppFooter.svelte';
 </script>
 
 <AppHeader />
-
 <slot />
-
 <AppFooter />
