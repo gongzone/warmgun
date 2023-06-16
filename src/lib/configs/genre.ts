43b0403ad20563837be6e91c663bdc688af46e76
dnjsqhwo <dnjsqhwo@gmail.com>
2023-06-16 22:07:35 +0900

    fix: genre href 수정

--- src/lib/configs/genre.ts
+++ src/lib/configs/genre.ts
@@ -2,43 +2,43 @@ export const genre = [
 	{
 		title: '프론트엔드',
 		enum: 'FRONTEND',
-		href: '/articles?genre=frontend',
+		href: '/articles/trending/frontend',
 		slug: 'frontend'
 	},
 	{
 		title: '백엔드',
 		enum: 'BACKEND',
-		href: '/articles?genre=backend',
+		href: '/articles/trending/backend',
 		slug: 'backend'
 	},
 	{
 		title: '데브옵스',
 		enum: 'DEVOPS',
-		href: '/articles?genre=devops',
+		href: '/articles/trending/devops',
 		slug: 'devops'
 	},
 	{
 		title: '모바일',
 		enum: 'MOBILE',
-		href: '/articles?genre=mobile',
+		href: '/articles/trending/mobile',
 		slug: 'mobile'
 	},
 	{
 		title: '데이터 사이언스',
 		enum: 'DATA_SCIENCE',
-		href: '/articles?genre=data_science',
+		href: '/articles/trending/data_science',
 		slug: 'data_science'
 	},
 	{
 		title: '게임',
 		enum: 'GAME',
-		href: '/articles?genre=game',
+		href: '/articles/trending/game',
 		slug: 'game'
 	},
 	{
 		title: '기타',
 		enum: 'ETC',
-		href: '/articles?genre=etc',
+		href: '/articles/trending/etc',
 		slug: 'etc'
 	}
 ];
