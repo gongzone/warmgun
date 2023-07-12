b9ee2a46220f9f55037138bffeddafc3e7f7850b
gongzone <dnjsqhwo@gmail.com>
2023-07-12 22:14:58 +0900

    chore: skeleton -> custom design system 변경 전

--- src/routes/(base)/+page.svelte
+++ src/routes/(base)/+page.svelte
@@ -2,6 +2,7 @@
 	import type { PageData } from './$types';
 
 	import Seo from '$components/@utils/Seo.svelte';
+	import Button from '$components/@ui/Test/Button.svelte';
 	import HomeHero from './_HomeHero/HomeHero.svelte';
 	import HomeTrendingArticles from './_HomeTrendingArticles/HomeTrendingArticles.svelte';
 	import HomeMoreInfo from './_HomeMoreInfo/HomeMoreInfo.svelte';
@@ -14,6 +15,7 @@
 <Seo />
 
 <div class="page-container space-y-28 md:space-y-40">
+	<Button>sd</Button>
 	<section>
 		<HomeHero {topUsers} {popularTags} />
 	</section>
