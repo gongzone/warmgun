87ce7af14a2ea3443c707b1673921f1bd3e998a3
gongzone <dnjsqhwo@gmail.com>
2023-08-21 13:01:24 +0900

    chore: Give Hidden component  more functionality

--- src/components/@ui/layout/hidden.tsx
+++ src/components/@ui/layout/hidden.tsx
@@ -4,12 +4,19 @@ import { tv, type VariantProps } from "tailwind-variants"
 
 const hiddenVariants = tv({
   variants: {
-    when: {
+    whenShowUp: {
       sm: "hidden sm:block",
       md: "hidden md:block",
       lg: "hidden lg:block",
       xl: "hidden xl:block",
       "2xl": "hidden 2xl:block",
+    },
+    whenHide: {
+      sm: "block sm:hidden",
+      md: "block md:hidden",
+      lg: "block lg:hidden",
+      xl: "block xl:hidden",
+      "2xl": "block 2xl:hidden",
     },
   },
 })
@@ -22,15 +29,16 @@ interface HiddenProps
 }
 
 const Hidden = React.forwardRef<HiddenElement, HiddenProps>(
-  ({ className, when, asChild = false, ...props }, forwardedRef) => {
+  (
+    { className, whenShowUp, whenHide, asChild = false, ...props },
+    forwardedRef
+  ) => {
     const Comp = asChild ? Slot : "div"
-    return (
-      <Comp
-        className={hiddenVariants({ when, className })}
-        ref={forwardedRef}
-        {...props}
-      />
-    )
+    const hiddenClasses =
+      whenShowUp && whenHide
+        ? hiddenVariants({ whenShowUp, className })
+        : hiddenVariants({ whenShowUp, whenHide, className })
+    return <Comp className={hiddenClasses} ref={forwardedRef} {...props} />
   }
 )
 Hidden.displayName = "Hidden"
